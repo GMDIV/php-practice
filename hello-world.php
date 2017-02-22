d0a0ff6097e714f9177998dda2fdc63d6e200c5e
Mitchell Deane <mitchelld20@yahoo.com>
2017-02-22 12:23:35 -0600

    went over arrays, numbers, and functions

--- hello-world.php
+++ hello-world.php
@@ -20,8 +20,48 @@
 		echo "$name has $hats hats </br>";
 
 		define("NAME", "James"); //constants can not be changed, unlike $ variables
-		echo NAME; //Will display "James"
+		echo NAME . "</br>"; //Will display "James"
 		
+		$number = 1;
+		$number++;  
+		echo $number ."</br>"; //2
+		$number--;
+		$number--;
+		$number--;
+		echo $number ."</br>"; //-1
+
+		$shapes = array(); //array example 1
+		$shapes[0] = "square";
+		$shapes[1] = "circle";
+		$shapes[2] = "triangle";
+
+		echo $shapes[1]."</br>"; //circle;
+
+		$shapes2 = ["square", "circle", "triangle"]; //array example 2
+
+		echo $shapes2[2]."</br>"; //triangle
+		echo count($shapes2)."</br>"; //Gives the number of items in the array (3)
+
+		$lastShape = array_pop($shapes2); //removes the last item in an array and returns the value. In this case that value is being stored within $lastShape. 
+		echo $lastShape."</br>"; //triangle
+		echo count($shapes2)."</br>";// 2
+		echo $lastShape."</br>";// triangle. Calling lastShape doesn't run array_pop again. It just gives the value that was originally returned and assigned to it.
+		echo count($shapes2)."</br>"; // 2
+
+		$numbers = [1, 3, 88, 22, 29];
+		echo max($numbers)."</br>"; // 88
+		echo min($numbers)."</br>"; // 1
+
+		function myFunction() {
+			echo "echoing a string from a function"."</br>";
+
+		}
+		myFunction(); //run the function, runing the echo
+
+		function multiply($a, $b){
+			echo $a * $b;
+		}
+		multiply(12, 3); //36
 	?>
 </body>
 </html>
