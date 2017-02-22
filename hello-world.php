bac83d7b9c97516f67651eba1e704bc0df6044d6
Mitchell Deane <mitchelld20@yahoo.com>
2017-02-22 11:59:38 -0600

    A couple displays of how strings/variables work

--- hello-world.php
+++ hello-world.php
@@ -10,7 +10,17 @@
 
 		echo "<h1>Hello World</h1>";
 		echo "<h2>Hello World</h2>";
+		echo strrev("Reversed String") . "</br>"; //displays a string in reverse order 
+		//   . joins two variables/strings/values. </br> is neccisary for line breaks  
+		echo strtoupper("uppercase string </br>"); //Capitalizes all letters in a string
+		echo strtolower("LOWERCASE STRING is Turned Into all lower CASE LeTters. </br>");
 
+		$hats = 8;
+		$name = "Jake";
+		echo "$name has $hats hats </br>";
+
+		define("NAME", "James"); //constants can not be changed, unlike $ variables
+		echo NAME; //Will display "James"
 		
 	?>
 </body>
