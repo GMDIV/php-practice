08c6860e66b0b703e4daefe9813fdd3599c83532
Mitchell Deane <mitchelld20@yahoo.com>
2017-02-21 19:36:26 -0600

    Getting started with a basic hello world

---
+++ hello-world.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title><?<?php echo "Hello World in PHP" ?></title>
+</head>
+<body>
+	<?php
+
+		echo "<h1>Hello World</h1>";
+		echo "<h2>Hello World</h2>";
+
+		
+	?>
+</body>
+</html>
